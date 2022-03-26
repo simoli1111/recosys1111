03d6a52c11cf7cca7b602a4fc72274735dc42785
simoli1111 <52670137+simoli1111@users.noreply.github.com>
2022-03-25 18:03:55 -0700

    Update Framework_EvaluationData.py

--- Framework_EvaluationData.py
+++ Framework_EvaluationData.py
@@ -41,9 +41,7 @@ class EvaluationData:
         anti_testset = []
         u = trainset.to_inner_uid(str(testSubject))
         user_items = set([j for (j, _) in trainset.ur[u]])
-        anti_testset += [(trainset.to_raw_uid(u), trainset.to_raw_iid(i), fill) for
-                                 i in trainset.all_items() if
-                                 i not in user_items]
+        anti_testset += [(trainset.to_raw_uid(u), trainset.to_raw_iid(i), fill) for i in trainset.all_items() if i not in user_items]
         return anti_testset
 
     def GetTrainSet(self):
