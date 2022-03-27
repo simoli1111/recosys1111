9c36ef2b2cee3523d5d9544d1801d960eae89e3b
simoli1111 <52670137+simoli1111@users.noreply.github.com>
2022-03-27 15:34:31 -0700

    Update ContentRecs.py

--- ContentRecs.py
+++ ContentRecs.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May  4 16:25:39 2018
-
-@author: Frank
-"""
-
-
 from MovieLens import MovieLens
 from ContentKNNAlgorithm import ContentKNNAlgorithm
 from Evaluator import Evaluator
